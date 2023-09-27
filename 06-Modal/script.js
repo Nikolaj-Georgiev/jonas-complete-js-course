6b8203cbee597fd7096d31d0695321cf9bec2666
NGNikolay <georgiev.nikolaj@gmail.com>
2023-09-27 09:18:27 +0300

    modified

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -22,3 +22,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
