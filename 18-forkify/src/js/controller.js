dfdf402c4c635c905b19b00ff7c51294756643ce
NGNikolay <georgiev.nikolaj@gmail.com>
2023-11-04 14:33:11 +0200

    Added new welcome feature

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -132,6 +132,10 @@ const controlAddRecipe = async function (newRecipe) {
 
 };
 
+const newFeature = function () {
+  console.log('Welcome to the app');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarksView);
   recipeView.addHandlerRender(controlRecipes);
@@ -140,5 +144,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 }
 init()
