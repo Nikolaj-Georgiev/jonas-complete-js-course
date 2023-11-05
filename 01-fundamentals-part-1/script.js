78e07e91bed3df2f6908238cb92ef2f899b35211
NGNikolay <georgiev.nikolaj@gmail.com>
2023-11-05 21:51:24 +0200

    added sections

---
+++ 01-fundamentals-part-1/script.js
@@ -0,0 +1,7 @@
+const favorite = Number(prompt('What the fuck was that???'));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+  console.log('Cool! 23 is an amazing number!');
+}
